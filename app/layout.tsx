827ba19e36fd3b8ba76b32012f79adf9c758b711
onoto-studio <128829377+onoto-studio@users.noreply.github.com>
2025-04-29 13:55:22 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,12 @@
-import "styles/tailwind.css"
+import "styles/tailwind.css";
+import { GeistSans } from '@vercel/font/geist-sans';
+
+const geist = GeistSans(); // Initialise la police Geist
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className={geist.className}> {/* Applique la classe Geist ici */}
       <body>{children}</body>
     </html>
-  )
+  );
 }
