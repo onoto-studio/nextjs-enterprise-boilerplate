2cb6fb377b6e326cbbab3abed35722f44c9c5315
onoto-studio <128829377+onoto-studio@users.noreply.github.com>
2025-04-29 14:30:13 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
       {
         width: 1200,
         height: 630,
-        url: "https://raw.githubusercontent.com/Blazity/next-enterprise/main/.github/assets/project-logo.png",
+        url: ".github/assets/ONOTO.gif",
       },
     ],
   },
@@ -26,9 +26,11 @@ export default function Web() {
       <section className="bg-white dark:bg-gray-900">
         <div className="mx-auto grid max-w-(--breakpoint-xl) px-4 py-8 text-center lg:py-16">
           <div className="mx-auto place-self-center">
-            <h1 className="mb-4 max-w-2xl text-4xl leading-none font-extrabold tracking-tight md:text-5xl xl:text-6xl dark:text-white">
-              ONOTO
-            </h1>
+             <img 
+              src="/assets/ONOTO.gif" 
+              alt="ONOTO Studio" 
+              className="mb-4 max-w-2xl mx-auto" 
+            />
             <p className="mb-6 max-w-2xl font-light text-gray-500 md:text-lg lg:mb-8 lg:text-xl dark:text-gray-400">
               Je vous accompagne dans la mise en forme graphique et numérique de votre idée, en alliant créativité et structure. 
               <br />Conception d’identités visuelles, création de logos, élaboration de chartes graphiques, design de sites vitrines, flyers, cartes de visite et bien plus encore.
